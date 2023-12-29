c8aae03df51fbd0b10a5ca329ff7e3869780ba60
JNSFilipe <jose.filipe@ieee.org>
2023-12-29 01:18:32 +0000

    Applied corrections

--- problems/P01.lisp
+++ problems/P01.lisp
@@ -1,7 +1,10 @@
 (defun my-last (l)
-  (if (eq (cdr l) nil)  ;; check when cdr of l is nil
-    l                   ;; when it is, the list only contains the last element, so return it
-    (my-last (cdr l)))  ;; When it is not, apply my-last recursively to the (cdr l) elements of the list
+  (if (null l)            ;; FIX: must first test if l is null to beggin with
+    nil                   ;; FIX: if so, return null
+    (if (null (rest l))   ;; check when cdr of l is nil FIX: can use rest insteal of cdr and (null (rest l)) instead of (eq ( rest l ) nil)
+      l                   ;; when it is, the list only contains the last element, so return it
+      (my-last (rest l)))  ;; When it is not, apply my-last recursively to the (cdr l) elements of the list
+  )
 )
 
 (my-last '(a b c d))
