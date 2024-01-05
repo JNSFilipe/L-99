3f61db5ca122d0435c654569eba9e19fd8216282
JNSFilipe <jose.filipe@ieee.org>
2024-01-05 02:21:19 +0000

    Corrected problem 2

--- problems/P02.lisp
+++ problems/P02.lisp
@@ -1,10 +1,13 @@
 (defun my-but-last (l)
-  (if (null l)
-    nil
-    (if (null (rest (rest l)))
+  ;; (if  l
+  (when l ;; Same as if l, but returning nill on else is implicit;
+    (if (null (cddr l))
       l
-      (my-but-last (rest l)))
+      (my-but-last (cdr l)))
+    ;; nil
   )
+  ;; (when cdr )
 )
 
 (my-but-last '(a b c d))
+(my-but-last '())
