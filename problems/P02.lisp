19f847a85dbcb5097019ef85d1824269be5ff620
JNSFilipe <jose.filipe@ieee.org>
2024-01-05 02:23:52 +0000

    Corrected Problem 2

--- problems/P02.lisp
+++ problems/P02.lisp
@@ -1,13 +1,10 @@
 (defun my-but-last (l)
-  ;; (if  l
-  (when l ;; Same as if l, but returning nill on else is implicit;
-    (if (null (cddr l))
-      l
-      (my-but-last (cdr l)))
-    ;; nil
+  (when                      ;; Same as if l, but returning nill on else is implicit;
+    (cdr l)                  ;; (cdr l) because should return nil if the list only has one element
+    (if (null (cddr l))      ;; (cddr l) is the same as (cdr (cdr l)). Also works with car and caar
+      l                      ;; return list if it has only two elements
+      (my-but-last (cdr l))) ;; Recursion
   )
-  ;; (when cdr )
 )
 
 (my-but-last '(a b c d))
-(my-but-last '())
